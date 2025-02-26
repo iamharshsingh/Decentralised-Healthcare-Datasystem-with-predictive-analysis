6474c650b3c3063dc98074c80d5d25dc589ba624
anandstomar <anandsinghtomar25.com>
2025-02-27 01:14:03 +0530

    lkjoifdjnjk

--- BACKEND2/server/app.py
+++ BACKEND2/server/app.py
@@ -6,7 +6,8 @@ import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True , origins=['https://render-2.vercel.app'])
+
 
 @app.route('/predict/heart', methods=['OPTIONS'])
 def options():
